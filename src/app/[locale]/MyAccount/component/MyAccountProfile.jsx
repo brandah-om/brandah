478a45ad7ab24477cfa1774b747d8ffa1907dd41
sayedosama7 <sayedosama088@gmail.com>
2025-03-27 20:47:22 +0200

    dashboard

--- src/app/[locale]/MyAccount/component/MyAccountProfile.jsx
+++ src/app/[locale]/MyAccount/component/MyAccountProfile.jsx
@@ -3,17 +3,18 @@ import React, { useEffect, useState } from 'react';
 import style from '../MyAccount.module.css';
 import { useTranslations } from 'next-intl';
 import Loading from '@/components/Loading/Loading';
-import { useRouter } from 'next/navigation'; // استيراد useRouter من next/navigation
+import { useRouter } from 'next/navigation';
 
 const MyAccountProfile = ({ data, isLoading, error }) => {
     const t = useTranslations('HomePage');
-    const router = useRouter(); // تهيئة useRouter
+    const router = useRouter();
 
     const [userData, setUserData] = useState({
         firstName: '',
         lastName: '',
         email: '',
         phone: '',
+        name: '',
     });
 
     useEffect(() => {
@@ -23,6 +24,7 @@ const MyAccountProfile = ({ data, isLoading, error }) => {
                 lastName: data.user.last_name,
                 email: data.user.email,
                 phone: data.user.phone,
+                name: data.user.name,
             });
         }
     }, [data]);
@@ -41,57 +43,74 @@ const MyAccountProfile = ({ data, isLoading, error }) => {
                         <p>{t('Error loading Data')}</p>
                     ) : (
                         <>
-                            <div className="col-md-6 d-flex flex-column mb-3">
-                                <label className={`${style.label}`}>
-                                    {t('First Name')} <span>*</span>
-                                </label>
-                                <input
-                                    className={style.contactInput}
-                                    type="text"
-                                    value={userData.firstName}
-                                    readOnly
-                                />
-                            </div>
-                            <div className="col-md-6 d-flex flex-column mb-3">
-                                <label className={`${style.label}`}>
-                                    {t('Last Name')} <span>*</span>
-                                </label>
-                                <input
-                                    className={style.contactInput}
-                                    type="text"
-                                    value={userData.lastName}
-                                    readOnly
-                                />
-                            </div>
-                            <div className="col-md-6 d-flex flex-column mb-3">
-                                <label className={`${style.label}`}>
-                                    {t('Email')} <span>*</span>
-                                </label>
-                                <input
-                                    className={style.contactInput}
-                                    type="email"
-                                    value={userData.email}
-                                    readOnly
-                                />
-                            </div>
-                            <div className="col-md-6 d-flex flex-column mb-3">
-                                <label className={`${style.label}`}>
-                                    {t('Phone Number')} <span>*</span>
-                                </label>
-                                <input
-                                    className={style.contactInput}
-                                    type="text"
-                                    value={userData.phone}
-                                    readOnly
-                                />
-                            </div>
-
-                            {data?.user.type === 'tour_guide' && (
+                            {userData.name && (
+                                <div className="col-md-6 d-flex flex-column mb-3">
+                                    <label className={`${style.label}`}>
+                                        {t('Full Name')} <span>*</span>
+                                    </label>
+                                    <input
+                                        className={style.contactInput}
+                                        type="text"
+                                        value={userData.name}
+                                        readOnly
+                                    />
+                                </div>
+                            )}
+                            {userData.firstName && (
+                                <div className="col-md-6 d-flex flex-column mb-3">
+                                    <label className={`${style.label}`}>
+                                        {t('First Name')} <span>*</span>
+                                    </label>
+                                    <input
+                                        className={style.contactInput}
+                                        type="text"
+                                        value={userData.firstName}
+                                        readOnly
+                                    />
+                                </div>
+                            )}
+                            {userData.lastName && (
+                                <div className="col-md-6 d-flex flex-column mb-3">
+                                    <label className={`${style.label}`}>
+                                        {t('Last Name')} <span>*</span>
+                                    </label>
+                                    <input
+                                        className={style.contactInput}
+                                        type="text"
+                                        value={userData.lastName}
+                                        readOnly
+                                    />
+                                </div>
+                            )}
+                            {userData.email && (
+                                <div className="col-md-6 d-flex flex-column mb-3">
+                                    <label className={`${style.label}`}>
+                                        {t('Email')} <span>*</span>
+                                    </label>
+                                    <input
+                                        className={style.contactInput}
+                                        type="email"
+                                        value={userData.email}
+                                        readOnly
+                                    />
+                                </div>
+                            )}
+                            {userData.phone && (
+                                <div className="col-md-6 d-flex flex-column mb-3">
+                                    <label className={`${style.label}`}>
+                                        {t('Phone Number')} <span>*</span>
+                                    </label>
+                                    <input
+                                        className={style.contactInput}
+                                        type="text"
+                                        value={userData.phone}
+                                        readOnly
+                                    />
+                                </div>
+                            )}
+                            {data?.user.type !== 'user' && (
                                 <div className="col-12 text-center mt-3">
-                                    <button
-                                        onClick={handleGoToDashboard}
-                                        className={style.goTo}
-                                    >
+                                    <button onClick={handleGoToDashboard} className={style.goTo}>
                                         {t('Go to Dashboard')}
                                     </button>
                                 </div>
