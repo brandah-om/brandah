90f3c6d8a4147d36cdc3a43580661a30bedb944b
sayedosama7 <sayedosama088@gmail.com>
2025-04-06 21:59:44 +0200

    layout

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -22,6 +22,8 @@ const tajawal = Tajawal({
   display: 'swap',
 });
 
+
+
 export const metadata = {
   title: 'Brandah',
   description: 'Brandah Dream, Explore, Discover Your Travel Begins Here',
@@ -48,20 +50,13 @@ export default async function Layout({ children, params }) {
   }
 
   const direction = locale === 'ar' ? 'rtl' : 'ltr';
+  // const fontClass = locale === 'ar' ? tajawal.className : baloo.className;
   const fontClass = locale === 'ar' ? tajawal.className : baloo.className;
   const bodyFontClass = locale === 'ar' ? tajawal.className : baloo.className;
 
+
   return (
     <html lang={locale} className={fontClass} dir={direction}>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </head>
       <body className={bodyFontClass}>
         <ClientProviders messages={messages} locale={locale}>
           <ToastContainer
